22de811bd30dbf9d141137322eb78cec2a6c5ba2
HyunJae_K <hj@HyunJae-Kui-MacBookPro.local>
2020-07-06 22:52:41 +0900

    add test file

---
+++ test.c
@@ -0,0 +1,15 @@
+//
+//  test.c
+//  Code_Up
+//
+//  Created by HyunJae_K on 2020/07/06.
+//  Copyright © 2020 K. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!");
+    
+    return 0;
+}
