b7f9f4ac91c23315157341688388cf9b919f7bca
HyunJae_K <hj@HyunJae-Kui-MacBookPro.local>
2020-07-26 21:00:48 +0900

    20/07/26

---
+++ Basic 1/1101.c
@@ -0,0 +1,15 @@
+//
+//  1101.c
+//  Code_Up
+//
+//  Created by HyunJae_K on 2020/07/26.
+//  Copyright © 2020 K. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    
+    return 0;
+}
---
+++ Basic 2/1110.c
@@ -0,0 +1,18 @@
+//
+//  1110.c
+//  Code_Up
+//
+//  Created by HyunJae_K on 2020/07/26.
+//  Copyright © 2020 K. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    printf("%d", n);
+    
+    return 0;
+}
